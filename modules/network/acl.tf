725d7867972c0b78cfaafb57a65fdd4f69736dff
ami <amidamri12@gmail.com>
2025-06-07 20:12:49 +0300

    chore: - change icmp ports in acl for public and database - add ingress ssh protocol for public acl

--- modules/network/acl.tf
+++ modules/network/acl.tf
@@ -24,7 +24,7 @@ resource "aws_network_acl" "acl_public" {
     action     = "allow"
     cidr_block = local.database_subnets_map["${each.key}_database"].cidr
     from_port  = 0
-    to_port    = 0
+    to_port    = 255
   }
   ingress {
     protocol   = "icmp"
@@ -32,7 +32,15 @@ resource "aws_network_acl" "acl_public" {
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
-    to_port    = 0
+    to_port    = 255
+  }
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 2
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
   }
 
   tags = merge(var.tags, {
@@ -56,7 +64,7 @@ resource "aws_network_acl" "acl_database" {
     action     = "allow"
     cidr_block = local.public_subnets_map["${each.key}_public"].cidr
     from_port  = 0
-    to_port    = 0
+    to_port    = 255
   }
   ingress {
     protocol   = "tcp"
