aa1f515bc57a9df2439ccba567b865a6630dabe0
Ami <amidamri12@gmail.com>
2025-06-08 14:35:28 +0300

    refactor: rename local.tf to locals.tf in network module fix: - change acl and sg rules for both public and database subnets/ec2

--- modules/network/acl.tf
+++ modules/network/acl.tf
@@ -1,3 +1,6 @@
+#---------
+# Public
+#---------
 resource "aws_network_acl" "acl_public" {
   for_each = var.subnet_zones
   vpc_id   = var.vpc_id
@@ -19,12 +22,32 @@ resource "aws_network_acl" "acl_public" {
     to_port    = 22
   }
   egress {
-    protocol   = "icmp"
+    protocol   = "tcp"
     rule_no    = 3
     action     = "allow"
-    cidr_block = local.database_subnets_map["${each.key}_database"].cidr
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+  egress {
+    protocol   = "icmp"
+    rule_no    = 4
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
     from_port  = 0
-    to_port    = 255
+    to_port    = 0
+    icmp_type  = 0
+    icmp_code  = -1
+  }
+  egress {
+    protocol   = "icmp"
+    rule_no    = 5
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+    icmp_type  = 8
+    icmp_code  = -1
   }
   ingress {
     protocol   = "icmp"
@@ -32,15 +55,35 @@ resource "aws_network_acl" "acl_public" {
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
-    to_port    = 255
+    to_port    = 0
+    icmp_type  = 8
+    icmp_code  = -1
+  }
+  ingress {
+    protocol   = "icmp"
+    rule_no    = 2
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+    icmp_type  = 0
+    icmp_code  = -1
   }
   ingress {
     protocol   = "tcp"
-    rule_no    = 2
+    rule_no    = 3
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 22
     to_port    = 22
+  }
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 4
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
   }
 
   tags = merge(var.tags, {
@@ -54,6 +97,9 @@ resource "aws_network_acl_association" "acl_associate_public" {
   subnet_id      = aws_subnet.my_subnets["${each.key}_public"].id
 }
 
+#---------
+# Database
+#---------
 resource "aws_network_acl" "acl_database" {
   for_each = var.subnet_zones
   vpc_id   = var.vpc_id
@@ -64,7 +110,9 @@ resource "aws_network_acl" "acl_database" {
     action     = "allow"
     cidr_block = local.public_subnets_map["${each.key}_public"].cidr
     from_port  = 0
-    to_port    = 255
+    to_port    = 0
+    icmp_type  = 8
+    icmp_code  = -1
   }
   ingress {
     protocol   = "tcp"
@@ -73,6 +121,24 @@ resource "aws_network_acl" "acl_database" {
     cidr_block = local.public_subnets_map["${each.key}_public"].cidr
     from_port  = 22
     to_port    = 22
+  }
+  egress {
+    protocol   = "tcp"
+    rule_no    = 1
+    action     = "allow"
+    cidr_block = local.public_subnets_map["${each.key}_public"].cidr
+    from_port  = 1024
+    to_port    = 65535
+  }
+  egress {
+    protocol   = "icmp"
+    rule_no    = 2
+    action     = "allow"
+    cidr_block = local.public_subnets_map["${each.key}_public"].cidr
+    from_port  = 0
+    to_port    = 0
+    icmp_type  = 0
+    icmp_code  = -1
   }
 
   tags = merge(var.tags, {
--- modules/network/security_groups.tf
+++ modules/network/security_groups.tf
@@ -3,45 +3,28 @@ resource "aws_security_group" "sg" {
   name     = "${each.key}_sg"
   vpc_id = var.vpc_id
 }
+#--------------------------
+# Public
+#--------------------------
 
-resource "aws_vpc_security_group_ingress_rule" "allow_inbound_ssh_connection" {
-  count = 2
-  from_port         = 22
-  security_group_id = aws_security_group.sg["zone_${count.index + 1}_database"].id
-  referenced_security_group_id = aws_security_group.sg["zone_${count.index + 1}_public"].id
-  to_port           = 22
-  ip_protocol       = "tcp"
-  tags = {
-    Name = "Allow ssh connections from public subnet"
-  }
-  depends_on = [aws_security_group.sg]
-}
+#--------
+# Ingress
+#--------
 
-resource "aws_vpc_security_group_egress_rule" "allow_outbound_ssh_connection" {
-  count = 2
-  from_port         = 22
-  security_group_id = aws_security_group.sg["zone_${count.index + 1}_public"].id
-  referenced_security_group_id = aws_security_group.sg["zone_${count.index + 1}_database"].id
-  to_port           = 22
-  ip_protocol       = "tcp"
-  tags = {
-    Name = "Allow ssh connections to database subnet"
-  }
-  depends_on = [aws_security_group.sg]
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound_ssh_connection_public" {
   for_each          = local.public_subnets_map
   security_group_id = aws_security_group.sg[each.key].id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  from_port         = 22
+  cidr_ipv4 = "0.0.0.0/0"
+  to_port           = 22
+  ip_protocol       = "tcp"
   tags = {
-    Name = "Allow access to the internet"
+    Name = "Allow ssh connections to public subnet"
   }
   depends_on = [aws_security_group.sg]
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_all_icmp_to_public_subnet" {
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound_icmp_public" {
   for_each          = local.public_subnets_map
   security_group_id = aws_security_group.sg[each.key].id
   from_port         = -1
@@ -54,15 +37,69 @@ resource "aws_vpc_security_group_ingress_rule" "allow_all_icmp_to_public_subnet"
   depends_on = [aws_security_group.sg]
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_all_icmp_public_subnet_to_private" {
+#--------
+# Egress
+#--------
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  for_each          = local.public_subnets_map
+  security_group_id = aws_security_group.sg[each.key].id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  tags = {
+    Name = "Allow access to the internet"
+  }
+  depends_on = [aws_security_group.sg]
+}
+
+
+#--------------------------
+# Database
+#--------------------------
+
+#--------
+# Ingress
+#--------
+
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound_ssh_connection_database" {
+  count = 2
+  from_port         = 22
+  security_group_id = aws_security_group.sg["zone_${count.index + 1}_database"].id
+  referenced_security_group_id = aws_security_group.sg["zone_${count.index + 1}_public"].id
+  to_port           = 22
+  ip_protocol       = "tcp"
+  tags = {
+    Name = "Allow ssh connections from public subnet"
+  }
+  depends_on = [aws_security_group.sg]
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound_icmp_database" {
   count = 2
   security_group_id = aws_security_group.sg["zone_${count.index + 1}_database"].id
   referenced_security_group_id = aws_security_group.sg["zone_${count.index + 1}_public"].id
-  from_port         = -1
+  from_port         = 8
   to_port           = -1
   ip_protocol       = "icmp"
   tags = {
-    Name = "Allow pinging the database subnet from the public subnet"
+    Name = "Allow pinging the public subnet from outside the vpc"
+  }
+  depends_on = [aws_security_group.sg]
+}
+
+#--------
+# Egress
+#--------
+
+resource "aws_vpc_security_group_egress_rule" "allow_outbound_icmp_database" {
+  count = 2
+  security_group_id = aws_security_group.sg["zone_${count.index + 1}_database"].id
+  referenced_security_group_id = aws_security_group.sg["zone_${count.index + 1}_public"].id
+  from_port         = 0
+  to_port           = -1
+  ip_protocol       = "icmp"
+  tags = {
+    Name = "Allow pinging the public subnet from outside the vpc"
   }
   depends_on = [aws_security_group.sg]
 }
